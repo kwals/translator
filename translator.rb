73daa5c9da3d0a434fb2c800c63d9a5f0b7e1cc9
Mallory Bucell <m.f.bucell@gmail.com>
2015-02-27 13:57:58 -0500

    tweaks to route controllers

--- translator.rb
+++ translator.rb
@@ -12,15 +12,30 @@ class Spotibetical < Sinatra::Base
 
   enable :sessions, :method_override
 
+  def current_user
+    fail
+  end
+
   get '/' do
     erb :home
   end  
 
-  get '/shelves'
-    Item.where(#params)
+  get '/shelves'  
     erb :shelf
   end
 
+  post '/shelves'
+    @results = if params["original_language"]
+      Item.where(original_language: params["original_language"])
+    elsif params["translated_language"]
+      Item.where(translated_language: params["translated_language"])
+    elsif params["genre"]
+      Item.where(genre: params["genre"])
+    else
+      session[:error_message = "Could not find any items from that request. Please try again."]
+    end 
+    erb :shelf
+  end
 
   #ITEM ROUTES
   get '/Item'
@@ -28,16 +43,16 @@ class Spotibetical < Sinatra::Base
   end
 
   post '/Item'
-    x = Item.create!(#language_to, #language_from, #user_id, #original_text, #translated_text)
-    redirect "/Item/#{x}"
+    x = current_user.create_item!(language_from: params["language_from"], language_to: params["language_to"], original_text: params["original_text"], translated_text: params["translated_text"], genre: params["genre"], user_content: params["user_content"])
+    redirect "/Item/#{x.id}"
   end
 
   get '/Item/:id'
-    erb :item
+    erb :view_item
   end
 
-  post '/Item/:id'
-    Comment.create!(#... item_id: params[:id])
+  post '/Item/:id' #update
+    User.(find)session[user_id:].comment!(params[:id])
   end
 
   #USER ROUTES
