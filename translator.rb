16067d463a43a6d2128a175bd558c4015f232626
Mallory Bucell <m.f.bucell@gmail.com>
2015-02-27 15:36:53 -0500

    fix missing 'do's and add instance vars per discussion of functionality of routes

--- translator.rb
+++ translator.rb
@@ -20,17 +20,17 @@ class Translator < Sinatra::Base
     erb :home
   end  
 
-  get '/shelves'  
+  get '/shelves' do 
     erb :shelf
   end
 
-  post '/shelves'
+  post '/shelves' do
     @results = if params["original_language"]
-      Item.where(original_language: params["original_language"])
+      Item.where(original_language: params["original_language"]).id
     elsif params["translated_language"]
-      Item.where(translated_language: params["translated_language"])
+      Item.where(translated_language: params["translated_language"]).id
     elsif params["genre"]
-      Item.where(genre: params["genre"])
+      Item.where(genre: params["genre"]).id
     else
       session[:error_message = "Could not find any items from that request. Please try again."]
     end 
@@ -38,39 +38,44 @@ class Translator < Sinatra::Base
   end
 
   #ITEM ROUTES
-  get '/Item'
+  get '/item' do
+    @item = Item.find(params["id"])
     erb :create_item
   end
 
-  post '/Item'
+  post '/item' do
     x = current_user.create_item!(original_text: params["original_text"], translated_text: params["translated_text"], user_content: params["user_content"], original_language: params["original_language"], translated_language: params["translated_language"], genre: params["genre"])
-    redirect "/Item/#{x.id}"
+    redirect "/item/#{x.id}"
   end
 
-  get '/Item/:id'
+  get '/item/:id' do
+    @item = Item.find(params["id"])
     erb :view_item
   end
 
-  post '/Item/:id'
+  post '/item/:id' do
     current_user.comment(params["item_id"], params["content"])
   end
 
   #USER ROUTES
-  get '/User/:id'
+  get '/user/:id' do
+    @user = User.find(params["id"])
     erb :user_profile
   end
 
-  get '/User/:id/edit'
+  get '/user/:id/edit' do
+    @user = User.find(params["id"])
     erb :edit_profile
   end
 
-  patch '/User/:id'
+  patch '/user/:id' do
+    @user = User.find(params["id"])
     fail
     #User.find(:id).update!(#params)
   end
 
   #COMMENT ROUTES
-  patch '/comment'
+  patch '/comment' do
     if params["action"] = "upvote"
       params["comment_id"].upvote!
     elsif params["action"] = "downvote"
