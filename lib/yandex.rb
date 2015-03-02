6dc7d7b62be5d280080e2b6ce47ea945c6f8f067
Emily Chu <Emily.H.Chu@gmail.com>
2015-03-01 19:13:21 -0500

    code freeze

--- lib/yandex.rb
+++ lib/yandex.rb
@@ -6,10 +6,6 @@ class Yandex
     header = {'key' => ENV['YANDEX_KEY']}
   end
 
-  # def self.get_languages 
-  #   list = HTTParty.get("https://translate.yandex.net/api/1.5/tr.json/getLangs")
-  # end
-
   def self.translate text, lang="en"
     response = HTTParty.post('https://translate.yandex.net/api/v1.5/tr.json/translate',
       body:{'key' => ENV['YANDEX_KEY'],
